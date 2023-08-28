ddbbab64ce82ca5f3da640dd09e0bbbf18e570f6
ueliton camargo <uelitonn6@gmail.com>
2023-08-28 10:31:58 -0300

    Component Summary finalizado

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+import { Sumarry } from "../Summary";
+import { Container } from "./styles";
+
+
+export function Dashboard() {
+    return (
+        <Container>
+           <Sumarry /> 
+        </Container>
+    )
+}
